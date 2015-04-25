caa0af50614da6706f9b907bc13e4cdf6b928ed3
Heart-travel <zcgacbc@gmail.com>
2015-04-25 21:53:05 +0800

    Add the action for touching a item

--- src/com/example/listviewdemo/MainActivity.java
+++ src/com/example/listviewdemo/MainActivity.java
@@ -7,7 +7,11 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 	
@@ -53,7 +57,15 @@ public class MainActivity extends ActionBarActivity {
 		ListView listView = (ListView) findViewById(R.id.list_view);
 		listView.setAdapter(adapter);
 		
-
+		listView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+			int position, long id) {
+			Fruit fruit = fruitList.get(position);
+			Toast.makeText(MainActivity.this, fruit.getName(),
+			Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
 
 	@Override
