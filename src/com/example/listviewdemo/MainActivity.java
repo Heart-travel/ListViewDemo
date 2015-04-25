9b019966ec01ece6dc8a7444a15459d58f5a89ff
Heart-travel <zcgacbc@gmail.com>
2015-04-25 21:15:37 +0800

    Add the pic and text items to ListView

--- src/com/example/listviewdemo/MainActivity.java
+++ src/com/example/listviewdemo/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.listviewdemo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends ActionBarActivity {
+	
+	private List<Fruit> fruitList = new ArrayList<Fruit>();
+	
+	private void initFruits() {
+		Fruit apple = new Fruit("Apple", R.drawable.apple_pic);
+		fruitList.add(apple);
+		Fruit banana = new Fruit("Banana", R.drawable.banana_pic);
+		fruitList.add(banana);
+		Fruit orange = new Fruit("Orange", R.drawable.orange_pic);
+		fruitList.add(orange);
+		Fruit watermelon = new Fruit("Watermelon", R.drawable.watermelon_pic);
+		fruitList.add(watermelon);
+		Fruit pear = new Fruit("Pear", R.drawable.pear_pic);
+		fruitList.add(pear);
+		Fruit grape = new Fruit("Grape", R.drawable.grape_pic);
+		fruitList.add(grape);
+		Fruit pineapple = new Fruit("Pineapple", R.drawable.pineapple_pic);
+		fruitList.add(pineapple);
+		Fruit strawberry = new Fruit("Strawberry", R.drawable.strawberry_pic);
+		fruitList.add(strawberry);
+		Fruit cherry = new Fruit("Cherry", R.drawable.cherry_pic);
+		fruitList.add(cherry);
+		Fruit mango = new Fruit("Mango", R.drawable.mango_pic);
+		fruitList.add(mango);		
+	}
 
 	private String[] data = { "Apple", "Banana", "Orange", "Watermelon",
 			"Pear", "Grape", "Pineapple", "Strawberry", "Cherry", "Mango",
@@ -18,10 +45,15 @@ public class MainActivity extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-				MainActivity.this, android.R.layout.simple_list_item_1, data);
-				ListView listView = (ListView) findViewById(R.id.list_view);
-				listView.setAdapter(adapter);
+/*		ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+				MainActivity.this, android.R.layout.simple_list_item_1, data);*/
+		
+		initFruits(); // 初始化水果数据
+		FruitAdapter adapter = new FruitAdapter(MainActivity.this, R.layout.fruit_item, fruitList);
+		ListView listView = (ListView) findViewById(R.id.list_view);
+		listView.setAdapter(adapter);
+		
+
 	}
 
 	@Override
